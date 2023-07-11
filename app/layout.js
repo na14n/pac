b499b0f95a2345c897f67c3ecd17b6641322d251
na14n <na14an.07@gmail.com>
2023-07-11 12:01:00 +0800

    Connected wpgraphql
    - Added wpgraphql to Wordpress
    - Added graphql to NextJS
    - Added apollo to NextJS
    - Added ApolloClient connection

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,29 @@ import './globals.css'
 import { Header } from '@/components'
 import { Inter } from 'next/font/google'
 
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
 const inter = Inter({ subsets: ['latin'] })
+
+const client = new ApolloClient({
+  uri: 'https://prosapac.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+client.query({
+  query: gql`
+    query GetPosts {
+      posts {
+        nodes {
+          date
+          title
+          content
+        }
+      }
+    }
+    `,
+  })
+.then((result) => console.log('RESPONSE: ', result.data.posts));
 
 export const metadata = {
   title: 'Pros-Apac Corporation',
@@ -14,10 +36,13 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="pac">
       <body className={inter.className}>
         <div>
-          <Header/>
+          <Header />
         </div>
         {children}
       </body>
     </html>
   )
 }
+
+
+
