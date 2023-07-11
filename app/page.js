b6f317eb9825ca1f8a1f871bfed4443b334ce844
na14n <na14an.07@gmail.com>
2023-07-11 09:37:48 +0800

    Removed missing content

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main class="w-full flex flex-col items-center justify-center">
       <div className='w-full h-[100vh] bg-cyan-400'>
-        <img className='skew-y-6' src='/suisei.png'></img>
       </div>
       <div className='w-full h-[100vh] bg-green-400'></div>
     </main>
