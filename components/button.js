ec47120cc5724691a2b654fdbf455e8c7e467028
na14n <106856208+na14n@users.noreply.github.com>
2023-07-20 15:37:04 +0800

    Button updates

--- components/button.js
+++ components/button.js
@@ -8,14 +8,15 @@ const Button = ({ type, name }) => {
             </button>
         ) : (type === 2) ? (
             (
-                <button className="group px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
+                <button className="group p-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
                     <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
                         <span className='text-[#F1F1F1] group-hover:text=[#FFFFFF]'>{name}</span>
+
                     </div>
                 </button>
             )
         ) : (
-            <button className="group p-1rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
+            <button className="group px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
                 <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
                     <span className='text-[#F1F1F1] group-hover:text=[#FFFFFF]'>{name}</span>
                     <Icon icon="ic:round-arrow-right-alt" width="24" height="24" className='pl-1 pr-1 group-hover:pl-2 group-hover:pr-0 transition-all' />
