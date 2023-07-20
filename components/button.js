c9effc1f3a5c939f088d318009cc4e51c032273e
na14n <106856208+na14n@users.noreply.github.com>
2023-07-20 15:36:17 +0800

    Button & Header updates
    - Added new Button variant
    - changed Header search bar width when expanded

--- components/button.js
+++ components/button.js
@@ -6,11 +6,19 @@ const Button = ({ type, name }) => {
             <button className="group ring-2 min-w-content ring-white py-1 px-2 rounded-md hover:ring-nav-orange grow-0 shrink-0">
                 <span className="text-sm font-semibold text-white group-hover:text-nav-orange">{name}</span>
             </button>
+        ) : (type === 2) ? (
+            (
+                <button className="group px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
+                    <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
+                        <span className='text-[#F1F1F1] group-hover:text=[#FFFFFF]'>{name}</span>
+                    </div>
+                </button>
+            )
         ) : (
-            <button className="group px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
+            <button className="group p-1rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
                 <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
                     <span className='text-[#F1F1F1] group-hover:text=[#FFFFFF]'>{name}</span>
-                    <Icon icon="ic:round-arrow-right-alt" width="24" height="24" className='pl-1 pr-1 group-hover:pl-2 group-hover:pr-0 transition-all'/>
+                    <Icon icon="ic:round-arrow-right-alt" width="24" height="24" className='pl-1 pr-1 group-hover:pl-2 group-hover:pr-0 transition-all' />
                 </div>
             </button>
         )
--- components/searchBar.js
+++ components/searchBar.js
@@ -3,8 +3,8 @@ import { Icon } from '@iconify-icon/react';
 
 const SearchBar = () => {
     return (
-        <div className="relative w-full h-content rounded-md py-2 px-1 flex gap-1 items-center">
-            <input type="text" placeholder="Search Products" className="block w-full px-2 py-1 bg-[#FCFCFC] ring-2 ring-[#FCFCFC] text-[#373737] text-sm focus:outline-none focus:ring-nav-orange rounded-md placeholder:text-black/40" />
+        <div className="relative w-full h-content rounded-md py-2 px-1 flex gap-4 items-center">
+            <input type="text" placeholder="Search Products..." className="block w-[140px] px-2 py-1 bg-[#FCFCFC] ring-2 ring-[#FCFCFC] text-[#373737] text-sm focus:outline-none focus:ring-nav-orange rounded-md placeholder:text-black/40" />
             <Icon icon="ic:round-search" width="24" height="24"/>
 
         </div>
