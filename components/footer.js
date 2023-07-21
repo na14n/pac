2f528ae15fa92454e5e2a9b677896e795b0a4329
na14n <na14an.07@gmail.com>
2023-07-21 11:43:58 +0800

    Responsive Design
    - Add Breakpoints to tailwind config

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,8 @@ import Button from './button';
 
 const Footer = () => {
     return (
-        <div className="w-full h-[52vh] bg-[#121212] flex flex-col items-center justify-center px-16 pb-2 pt-4">
-            <div className=" w-full h-full grow-1 flex items-center justify-center">
+        <div className="w-full h-content bg-[#121212] flex flex-col items-center justify-center px-16 pb-2 pt-4">
+            <div className=" w-full h-full grow-1 flex items-center justify-center lg:flex-row xs:flex-col">
                 <div className=" w-full h-full basis-1/4 flex flex-col justify-center items-center">
                     <div className=" w-full h-full flex flex-col justify-start pt-12 pb-4">
                         <img src="/pac-white.png" className='w-64'></img>
@@ -18,54 +18,23 @@ const Footer = () => {
                         <span>+63 912 3456 789</span>
                     </div>
                 </div>
-                <div className=" w-full h-full pt-12 px-12 basis-1/2 grid grid-cols-2">
-                    <div className="flex flex-col">
+                <div className=" w-full h-full lg:pt-12 lg:px-12 xs:py-4 basis-1/2 grid lg:grid-cols-2 xs:grid-cols-1">
+                    <div className="flex flex-col xs:py-4 lg:py-0">
                         <span className="text-[#fcfcfc] uppercase font-bold pb-1">about</span>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Careers</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">History</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Sales Agents</a>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col xs:py-4 lg:py-0">
                         <span className="text-[#fcfcfc] text-sm uppercase font-bold pb-1">events</span>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Trainings and Workshops</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Dental Conventions and Exhibits</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Seminars</a>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col xs:py-4 lg:py-0">
                         <span className="text-[#fcfcfc] text-sm uppercase font-bold pb-1">our products</span>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">PAC Exclusive Brands</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Orthodontics</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Aesthetics</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Restorative</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Implant</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Prostige Platinum</a>
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col xs:py-4 lg:py-0">
                         <span className="text-[#fcfcfc] text-sm uppercase font-bold pb-1">resources</span>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">How to order</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Sales Agents</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">Aesthetics</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">News and Updates</a>
-                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-[#fcfcfc] text-sm" href="#">E - Catalogues</a>
+                        <a className="text-[#e1e1e1]/90 underline-offset-2 hover:underline hover:text-pac-orange text-sm" href="#">How to order</a>
                     </div>
                 </div>
-                <div className=" w-full h-full basis-1/4 flex flex-col items-center">
+                <div className=" w-full h-full basis-1/4 flex flex-col items-center xs:gap-8 xs:py-6 lg:gap-4">
                     <div className=" w-full h-full flex gap-4 items-center text-[#fcfcfc]">
-                        <a href='#' className="hover:text-nav-orange w-content h-content flex justify-center items-center">
-                            <Icon icon="ri:facebook-fill" width="48" height="48" />
-                        </a>
-                        <a href='#' className="hover:text-nav-orange w-content h-content flex justify-center items-center">
-                            <Icon icon="ri:instagram-fill" width="48" height="48" />
-                        </a>
-                        <a href='#' className="hover:text-nav-orange w-content h-content flex justify-center items-center">
-                            <Icon icon="ri:tiktok-fill" width="48" height="48" />
-                        </a>
-                        <a href='#' className="hover:text-nav-orange w-content h-content flex justify-center items-center">
-                            <Icon icon="ri:youtube-fill" width="48" height="48" />
-                        </a>
-                        <a href='#' className="hover:text-nav-orange w-content h-content flex justify-center items-center">
-                            <Icon icon="ri:linkedin-fill" width="48" height="48" />
-                        </a>
                     </div>
                     <div className=" w-full h-full flex flex-col gap-2 items-start">
                         <span className='text-[#fcfcfc] text-sm font-semibold'>Already Know What to Order?</span>
@@ -77,9 +46,9 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="w-full min-h-[20px] h-[30px] max-h-[30px] shrink-1 border-t-2 border-[#e1e1e1]/90 px-12 py-1 flex items-center justify-between">
+            <div className="w-full h-content shrink-1 border-t-2 border-[#e1e1e1]/90 sm:px-0 py-1 flex items-start justify-between flex-row">
                 <span className="text-[#e1e1e1] text-xs ">All Rights Reeserved 2023</span>
-                <div className="flex gap-8">
+                <div className="flex xs:flex-col lg:flex-row lg:gap-4 xs:gap-2">
                     <a className="text-[#e1e1e1]/90 text-xs underline-offset-2 hover:underline hover:text-[#fcfcfc]" href="#">Data Protection</a>
                     <a className="text-[#e1e1e1]/90 text-xs underline-offset-2 hover:underline hover:text-[#fcfcfc]" href="#">Privacy Policy</a>
                 </div>
