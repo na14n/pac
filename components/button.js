06c6109d0378a4a49a762f8a1eb986aeb6cea6b3
na14n <na14an.07@gmail.com>
2023-07-28 17:01:39 +0800

    Download Button

--- components/button.js
+++ components/button.js
@@ -18,6 +18,13 @@ const Button = ({ type, name, link }) => {
             <button className="group ring-2 w-content ring-nav-orange py-3 px-5 rounded-md hover:ring-[#fcfcfc] grow-0 shrink-0">
                 <span className="text-sm font-semibold text-nav-orange group-hover:text-[#fcfcfc]">{name}</span>
             </button>
+        ) : (type === 'download') ? (
+            <a className="group w-fit px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500" target='_blank' href={link} download={name}>
+                <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
+                    <span className='text-[#F1F1F1] group-hover:text=[#FFFFFF]'>{name}</span>
+                    <Icon icon="mdi:file-download" width="24" height="24" className='pl-1' />
+                </div>
+            </a>
         ) : (
             <button className="group w-fit px-2 py-1 rounded-md bg-size-200 bg-pos-0 bg-gradient-to-b from-[#E05B25] via-[#FD8F29] to-[#E05B25] shadow-sm hover:shadow-xl hover:bg-pos-100 transition-all duration-500">
                 <div className="pl-1 text-sm font-semibold text-[#F1F1F1] flex items-center">
--- components/resources-pages/catalogueCard.js
+++ components/resources-pages/catalogueCard.js
@@ -6,7 +6,7 @@ const CatalogueCard = ({img, name, link}) => {
             <img className="w-full h-auto absolute top-0 z-0" src={link ? link : 'https://picsum.photos/1920/1080'}/>
             <div className="bg-[#EFEFEF] h-1/3 w-full z-10 flex flex-col py-2 px-4 gap-2">
                 <div className="font-bold text-[#121212] text-lg">{name ? name : 'Catalog Name'}</div>
-                <Button name={'Download'} />
+                <Button name={'Download'} type={'download'} link={'https://prosapac.com/wp-content/uploads/2023/07/sample.pdf'} />
             </div>
         </div>
     )
