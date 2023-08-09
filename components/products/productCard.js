2df69ffa341d981a7f6ba6582592863b55a79ea4
na14n <na14an.07@gmail.com>
2023-08-09 14:09:40 +0800

    Product Card Update
    - Added Best Seller design on Product Cards

--- components/products/productCard.js
+++ components/products/productCard.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 
-const ProductCard = ({media, name, brand, category, slug}) => {
+const ProductCard = ({ media, name, brand, category, slug, best }) => {
     return (
-        <a className="z-10 rounded-sm overflow-hidden bg-[#FCFCFC] shadow-sm w-64 h-72 relative  flex flex-col items-center justify-end group hover:-translate-y-1 hover:shadow-lg transition-all" href={slug ? slug : '/'}>
-            <Image width={256} height={256} src={media ? media : 'https://picsum.photos/2400'} alt="dental-product" />
-            <div className="w-full h-2/5 flex flex-col p-2">
+        <a className="z-10 rounded-sm overflow-hidden bg-[#FCFCFC] shadow-sm w-60 h-fit relative  flex flex-col items-center justify-end group hover:-translate-y-1 hover:shadow-lg transition-all" href={slug ? slug : '/'}>
+            <div className={`w-32 h-32 absolute rotate-45 -top-16 -right-16 z-10 bg-gradient-to-r from-nav-orange to-pac-orange px-3 py-2 flex flex-col justify-end items-center ${best == 'true' ? '' : 'hidden'}`}>
+                <p className="text-[#FCFCFC] capitalize font-bold text-xs">Best</p>
+                <p className="text-[#FCFCFC] capitalize font-bold text-xs">Seller</p>
+            </div>
+            <div className="w-full h-64 z-0 relative flex items-center justify-center">
+                <Image fill={true} src={media ? media : '/gengigel-bottle.png'} alt="dental-product" />
+            </div>
+            <div className="w-full h-20 flex flex-col p-2 z-10">
                 <div className="font-bold text-[#171717] text-lg">{name ? name : 'Product Name'}</div>
                 <div className="text-[#373737] text-xs">{brand ? brand : 'Brand Name'}</div>
                 <div className="text-[#373737] text-xs">{category ? category : 'Category'}</div>
