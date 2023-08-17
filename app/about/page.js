c4dd269e322b5397c6516b778bf64652bf81605a
Poncholo Riego de Dios <poncholo.riego.de.dios@adamson.edu.ph>
2023-08-17 16:20:54 +0800

    Code Spacing (About page)
    -Adjusted the spacing per individual block of code of the about page

--- app/about/page.js
+++ app/about/page.js
@@ -21,9 +21,13 @@ export default function page() {
           <MVC />
         </div>
       </div>
+
+
       <div className="w-full lg:h-[140vh] max-h-fit bg-pac-charcoal pt-2 pl-16">
         <Founder />
       </div>
+
+
       <div className="w-full lg:h-[230vh] max-h-fit bg-[url('https://scontent.fmnl3-2.fna.fbcdn.net/v/t1.15752-9/365538850_328863899572736_1827216221858357976_n.png?_nc_cat=105&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFe60aj2IfZ_XJOPMIPFfZk2sUWOrBem1_axRY6sF6bXwHLyIQB3g0yta9sVOdJgLTvXvbsj9bccbTrEXfbGQ4j&_nc_ohc=R51NyNu8G58AX_BvigF&_nc_oc=AQkYSXBXdqCorfXslGYu8tTMGQdSTnKxiX7XG2ncM4SJoVTyWSbfZrlWR23CdjzgUog&_nc_ht=scontent.fmnl3-2.fna&oh=03_AdTl_odYGhsgh5i4xrHDVUR8WFP-ulMHlKfzF2YK7DHj7w&oe=65014347')] bg-cover">
         <section>
         <Coolab />
@@ -32,6 +36,8 @@ export default function page() {
         <History />
         </section>
       </div>
+
+      
       <div className="w-full lg:h-[100vh] max-h-fit bg-[#F1F1F1]">
         <Facilities />
       </div>
