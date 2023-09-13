bad380683536bee6a0157f444d89435298b9b679
na14n <na14an.07@gmail.com>
2023-09-13 13:21:09 +0800

    Company Activities
    - Added Company Activities Page and Design
    - Added CSR Page

---
+++ app/about-us/csr/page.js
@@ -0,0 +1,7 @@
+export default function CsrPage() {
+    return(
+        <main>
+            <h1>CSR PAGE</h1>
+        </main>
+    )
+}
--- components/about/companyActivities.js
+++ components/about/companyActivities.js
@@ -6,19 +6,20 @@ import { sortByAttribute } from "@/lib/helpers";
 import Image from "next/image";
 import Button from "../button";
 import parse from 'html-react-parser'
+import CompanyActivitiesSlider from "../embla/companyActivitiesSlider";
 
 const query = gql`
-query fetchEventsLandingPage {
-    events(where: {title: "landing-page"}) {
+query fetchCompanyActivitiesPage {
+    aboutContents(where: {title: "company-activities"}) {
       nodes {
-        name
-        eventName
-        shortDescription
-        extraContentLine1
-        extraContentLine2
-        mediaLine1{
-          title
+        sectionTitle
+        sectionHeading
+        sectionSubheading
+        contentLine1
+        contentLine2
+        mediaLine2{
           sourceUrl
+          title
           altText
         }
       }
@@ -27,9 +28,38 @@ query fetchEventsLandingPage {
 `
 
 export default function CompanyActivitiesBanners() {
-    return(
-        <section className="w-full h-fit flex flex-col px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 py-16">
-            <div className="px-8 xl:px-16 2xl:px-32 w-full py-8 bg-[#FCFCFC]"></div>
-        </section>    
-    )
+
+  const { data } = useSuspenseQuery(query,
+    {
+      context: {
+        fetchOptions: {
+          next: { revalidate: 60 },
+        },
+      },
+    })
+
+  const assets = sortByAttribute(data?.aboutContents?.nodes[0]?.mediaLine2, 'title')
+
+  return (
+    <section className="w-full h-fit flex flex-col px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 py-16">
+      {data ? data?.aboutContents?.nodes[0]?.sectionSubheading.map((s, i) => (
+        <div key={i} className="flex w-full bg-[#FCFCFC]">
+          <div className="w-2/5 self-stretch p-8 flex flex-col gap-12">
+            <h1 className="text-3xl font-bold text-[#121212]">{s ? s : ``}</h1>
+            <div className="w-12 h-[2px] bg-[#373737] rounded-full" />
+            <p className="text-[#272727] text-justify">{data ? data?.aboutContents?.nodes[0]?.contentLine1[i] : ``}</p>
+          </div>
+          <div className={`w-3/5 aspect-[3/2] flex justify-center items-center relative  ${i % 2 === 0 ? `` : `order-first`}`}>
+            {/* <Image fill={true} src={assets.filter((item) => item.title.includes(`${data?.aboutContents?.nodes[0]?.contentLine2[i]}`))[2].sourceUrl} className="object-contain object-center" /> */}
+            <CompanyActivitiesSlider media={assets.filter((item) => item.title.includes(`${data?.aboutContents?.nodes[0]?.contentLine2[i]}`))} index={i} />
+          </div>
+        </div>
+      )) : ``}
+      <div className="w-full h-96 flex items-center justify-center bg-pac-green flex-col gap-8">
+        <h1 className="text-3xl font-bold text-[#FCFCFC]">CSR Page</h1>
+        <p className="text-[#EFEFEF]">Discover how we're contributing to a better future through meaningful initiatives and community engagement.</p>
+        <Button name={'Click Here'} link={`/about-us/csr`} type={1} />
+      </div>
+    </section>
+  )
 }
