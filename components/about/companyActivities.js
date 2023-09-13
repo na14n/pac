f58e6caa9efbeff0cb95fac10592c01c097b06c3
na14n <na14an.07@gmail.com>
2023-09-13 13:41:34 +0800

    Activities Page
    - Added Responsive Layout for Activities Page

--- components/about/companyActivities.js
+++ components/about/companyActivities.js
@@ -41,25 +41,24 @@ export default function CompanyActivitiesBanners() {
   const assets = sortByAttribute(data?.aboutContents?.nodes[0]?.mediaLine2, 'title')
 
   return (
-    <section className="w-full h-fit flex flex-col px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 py-16">
+    <section className="w-full h-fit flex flex-col px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 py-16 gap-8 lg:gap-0 ">
       {data ? data?.aboutContents?.nodes[0]?.sectionSubheading.map((s, i) => (
-        <div key={i} className="flex w-full bg-[#FCFCFC]">
-          <div className="w-2/5 self-stretch p-8 flex flex-col gap-12">
+        <div key={i} className="flex flex-col lg:flex-row w-full bg-[#FCFCFC] shadow-md lg:shadow-none">
+          <div className="w-full lg:w-2/5 self-stretch p-4 xl:p-8 flex flex-col gap-4 xl:gap-12">
             <h1 className="text-3xl font-bold text-[#121212]">{s ? s : ``}</h1>
             <div className="w-12 h-[2px] bg-[#373737] rounded-full" />
             <p className="text-[#272727] text-justify">{data ? data?.aboutContents?.nodes[0]?.contentLine1[i] : ``}</p>
           </div>
-          <div className={`w-3/5 aspect-[3/2] flex justify-center items-center relative  ${i % 2 === 0 ? `` : `order-first`}`}>
-            {/* <Image fill={true} src={assets.filter((item) => item.title.includes(`${data?.aboutContents?.nodes[0]?.contentLine2[i]}`))[2].sourceUrl} className="object-contain object-center" /> */}
+          <div className={`w-full lg:w-3/5 aspect-[3/2] flex justify-center items-center relative  ${i % 2 === 0 ? `` : `lg:order-first`}`}>
             <CompanyActivitiesSlider media={assets.filter((item) => item.title.includes(`${data?.aboutContents?.nodes[0]?.contentLine2[i]}`))} index={i} />
           </div>
         </div>
       )) : ``}
-      <div className="w-full h-96 flex items-center justify-center bg-pac-green flex-col gap-8">
+      <div className="w-full h-96 flex items-center justify-center bg-pac-green flex-col gap-8 shadow-md lg:shadow-none p-8">
         <h1 className="text-3xl font-bold text-[#FCFCFC]">
           {data?.aboutContents?.nodes[0] ? data?.aboutContents?.nodes[0]?.sectionHeading[1] : ``}
         </h1>
-        <p className="text-[#EFEFEF]">
+        <p className="text-[#EFEFEF] text-center">
           {data?.aboutContents?.nodes[0] ? data?.aboutContents?.nodes[0]?.sectionHeading[2] : ``}
         </p>
         <Button name={'Click Here'} link={`/about-us/csr`} type={1} />
--- components/embla/companyActivitiesSlider.js
+++ components/embla/companyActivitiesSlider.js
@@ -15,7 +15,7 @@ export default function CompanyActivitiesSlider({ media, index }) {
 
     return (
         <section className='relative w-full h-fit flex items-center justify-center'>
-            <button className={`p-2 flex items-center justify-center hover:shadow-md shadow-sm bg-[#FCFCFC]/75 hover:bg-[#FCFCFC]/100 rounded-full absolute ${index%2 === 0 ? `left-8` : `right-8`} bottom-8 z-30 transition-all`} onClick={scrollNext}>
+            <button className={`p-2 flex items-center justify-center hover:shadow-md shadow-sm bg-[#FCFCFC]/75 hover:bg-[#FCFCFC]/100 rounded-full absolute ${index%2 === 0 ? `left-8` : `left-8 lg:right-8`} bottom-8 z-30 transition-all`} onClick={scrollNext}>
                 <Icon icon="ic:round-keyboard-arrow-right" className=' text-3xl max-lg:text-xl text-nav-orange' />
             </button>
             <div className='overflow-hidden w-full aspect-[3/2]' ref={emblaRef}>
