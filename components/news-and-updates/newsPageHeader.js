164947aed113a942fb2036d572d3398a1da850dc
na14n <na14an.07@gmail.com>
2023-09-15 13:48:25 +0800

    News Page
    - Added Responsive Layout for News Page

--- components/news-and-updates/newsPageHeader.js
+++ components/news-and-updates/newsPageHeader.js
@@ -29,12 +29,12 @@ export default function NewsPageHeader({ id }) {
 
     return (
         <>
-            <section className="flex w-full h-fit px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 gap-8 pt-8 pb-16 bg-[#153f00]">
-                <div className="w-2/5 aspect-[3/2] shrink-0">
+            <section className="flex flex-col md:flex-row justify-center w-full h-fit px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 gap-8 pt-8 pb-16 bg-[#153f00]">
+                <div className="w-full md:w-2/5 aspect-[3/2] shrink-0 rounded-sm shadow-sm overflow-hidden">
                     <CsrSlider media={data ? data?.newsAndUpdate?.mediaLine : []} />
                 </div>
-                <div className="w-full self-stretch flex items-center justify-center">
-                    <h1 className="text-[#FCFCFC] font-bold text-3xl self-center text-justify">{data ? data?.newsAndUpdate?.name : ``}</h1>
+                <div className="w-fit self-stretch flex items-center justify-center">
+                    <h1 className="text-[#FCFCFC] font-bold text-3xl self-center text-justify lg:max-w-[20ch] xl:max-w-[24ch]">{data ? data?.newsAndUpdate?.name : ``}</h1>
                 </div>
             </section>
             <section className="flex w-full h-fit px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 gap-8 pt-8 pb-16 justify-center">
