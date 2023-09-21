9f9f8018063ef582e9f41e60713f9759f6fe2f53
na14n <na14an.07@gmail.com>
2023-09-21 10:14:20 +0800

    Trainings and Seminars
    - Changes to images sorting

---
+++ app/trainings-&-seminars/conventions/[id]/page.js
@@ -0,0 +1,16 @@
+import { HeaderTrigger } from "@/components";
+import ConHero from "@/components/trainings-&-seminars/conventions/conHero";
+
+export default function ConventionPage({ params }) {
+    return (
+        <main className="w-full min-h-screen h-fit flex flex-col items-center relative">
+            <HeaderTrigger>
+                <ConHero />
+            </HeaderTrigger>
+            <h1 className="text-xl font-bold text-black">Hello World</h1>
+            <pre>
+                {JSON.stringify(params, null, 4)}
+            </pre>
+        </main>
+    )
+}
