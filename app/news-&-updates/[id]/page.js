9a341c3ceb5546114fa23011e90089964ea00558
na14n <na14an.07@gmail.com>
2023-10-05 09:57:23 +0800

    Testing Dynamic Metadata on News Page

--- app/news-&-updates/[id]/page.js
+++ app/news-&-updates/[id]/page.js
@@ -1,7 +1,42 @@
 import { HeaderTrigger } from "@/components"
 import NewsPageHeader from "@/components/news-and-updates/newsPageHeader"
+import client from '@/lib/apollo';
+import { gql } from 'graphql-tag';
 
-export default function NewsPage({params}) {
+
+let pageData
+
+export async function generateMetadata({ params }, parent) {
+    try {
+        const { data } = await client.query({
+            query: gql`
+                    query fetchNewsHeader {
+                        newsAndUpdate(id: "${params?.id}=") {
+                        name
+                        mediaLine1 {
+                            title
+                            sourceUrl
+                        }
+                        }
+                    }
+          `,
+        });
+        pageData = data
+    } catch (error) {
+        console.error('Error occurred:', error);
+        pageData = []
+    }
+
+    return{
+        title: pageData?.newsAndUpdate?.name,
+        openGraph: {
+            images: [pageData?.newsAndUpdate?.mediaLine1[0].sourceUrl]
+        }
+    }
+
+}
+
+export default async function NewsPage({ params }) {
     return (
         <main className="w-full min-h-screen h-fit bg-[#EFEFEF] overflow-hidden">
             <HeaderTrigger>
--- components/news-and-updates/newsPageHeader.js
+++ components/news-and-updates/newsPageHeader.js
@@ -10,6 +10,8 @@ import Link from "next/link";
 import CsrSlider from "../embla/csrSlider";
 import parse from 'html-react-parser'
 import NewsPageRec from "./newsPageRec";
+import { useEffect, useState } from "react";
+
 
 
 export default function NewsPageHeader({ id }) {
@@ -27,7 +29,7 @@ export default function NewsPageHeader({ id }) {
       }
     `
 
-    const { data, } = useSuspenseQuery(
+    const { data } = useSuspenseQuery(
         query,
         {
             context: {
@@ -38,6 +40,18 @@ export default function NewsPageHeader({ id }) {
         }
     );
 
+    const [pageData, setPageData] = useState(null);
+
+    useEffect(() => {
+        setPageData(data);
+        if (pageData) {
+            // document.title = pageData?.newsAndUpdate?.name
+            
+        }
+    }, [data])
+
+
+
     return (
         <>
             <section className="flex flex-col px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 bg-[#153f00]">
@@ -45,20 +59,21 @@ export default function NewsPageHeader({ id }) {
             </section>
             <section className="flex flex-col md:flex-row justify-center w-full h-fit px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 gap-8 pt-8 pb-16 bg-[#153f00]">
                 <div className="w-full md:w-2/5 aspect-[3/2] shrink-0 rounded-sm shadow-sm overflow-hidden">
-                    <CsrSlider media={data ? data?.newsAndUpdate?.mediaLine1 : []} />
+                    <CsrSlider media={pageData ? pageData?.newsAndUpdate?.mediaLine1 : []} />
                 </div>
                 <div className="w-fit self-stretch flex items-center justify-center">
-                    <h1 className="text-[#FCFCFC] font-bold text-3xl self-center lg:max-w-[20ch] xl:max-w-[24ch]">{data ? data?.newsAndUpdate?.name : ``}</h1>
+                    <h1 className="text-[#FCFCFC] font-bold text-3xl self-center lg:max-w-[20ch] xl:max-w-[24ch]">{pageData ? pageData?.newsAndUpdate?.name : ``}</h1>
                 </div>
             </section>
             <section className="flex flex-col lg:flex-row w-full h-fit px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 gap-8 pt-8 pb-16 justify-center">
                 {/* <p className="b text-[#272727] max-w-[75ch] text-justify"> */}
                 <div className="b text-[#272727] max-w-[75ch] text-justify flex flex-col gap-4">
-                    {parse(data?.newsAndUpdate?.content)}
-                    {/* {data?.newsAndUpdate?.content} */}
+                    {/* {parse(pageData?.newsAndUpdate?.content)} */}
+                    {/* {pageData?.newsAndUpdate?.content} */}
+                    {pageData ? parse(pageData?.newsAndUpdate?.content) : 'Loading...'}
                 </div>
                 {/* </p> */}
-                <NewsPageRec id={id} rec={data?.newsAndUpdate?.relatedTags} />
+                {/* <NewsPageRec id={id} rec={pageData?.newsAndUpdate?.relatedTags} /> */}
             </section>
         </>
     )
