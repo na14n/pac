5066b2f593e5fa18ff31c9f36dbd1d0cd715dbba
na14n <na14an.07@gmail.com>
2023-10-18 11:58:12 +0800

    Conventions Page
    - Added Metadata
    - Added Social Media Sharing

--- app/trainings-&-seminars/conventions/[id]/page.js
+++ app/trainings-&-seminars/conventions/[id]/page.js
@@ -1,6 +1,53 @@
 import { HeaderTrigger } from "@/components";
 import ConGrid from "@/components/trainings-&-seminars/conventions/conGrid";
 import ConHero from "@/components/trainings-&-seminars/conventions/conHero";
+import client from '@/lib/apollo';
+import { partString } from "@/lib/helpers";
+import { gql } from 'graphql-tag';
+
+let pageData
+
+export async function generateMetadata({ params }) {
+    try {
+        const { data } = await client.query({
+            query: gql`
+            query fetchCovention($id: ID!) {
+                convention(id: $id) {
+                  eventName
+                  location
+                  shortDescription
+                  banner {
+                    sourceUrl
+                  }
+                }
+              }
+          `,
+            fetchPolicy: 'no-cache',
+            variables: {
+                "id": params.id
+            },
+            context: {
+                fetchOptions: {
+                    next: { revalidate: 60 },
+                },
+            },
+        });
+        pageData = data
+    } catch (error) {
+        console.error('Error occurred:', error);
+        pageData = []
+    }
+
+    return {
+        title: pageData?.convention?.eventName,
+        keywords: pageData ? partString(pageData?.convention.eventName) : "prosapac",
+        openGraph: {
+            images: [pageData?.convention?.banner?.sourceUrl]
+        },
+    }
+
+}
+
 
 export default function ConventionPage({ params }) {
     return (
@@ -8,7 +55,7 @@ export default function ConventionPage({ params }) {
             <HeaderTrigger>
                 <ConHero id={params.id} />
             </HeaderTrigger>
-           <ConGrid id={params.id} />
+            <ConGrid id={params.id} />
         </main>
     )
 }
--- components/trainings-&-seminars/conventions/conGrid.js
+++ components/trainings-&-seminars/conventions/conGrid.js
@@ -8,6 +8,8 @@ import { startTransition } from "react";
 import Image from "next/image";
 import ConGridItem from "./conGridItem";
 import { Icon } from "@iconify-icon/react";
+import { FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton, FacebookMessengerIcon, FacebookMessengerShareButton, ViberIcon, ViberShareButton } from "react-share"
+import { usePathname } from 'next/navigation'
 
 const query = gql`query FetchConvention($id: ID!) {
     convention(id: $id) {
@@ -20,6 +22,8 @@ const query = gql`query FetchConvention($id: ID!) {
   }`
 
 export default function ConGrid({ id }) {
+
+    const pathname = `${process.env.NEXT_PUBLIC_BASE_URL}${usePathname()}`
 
     const { data } = useSuspenseQuery(query,
         {
@@ -51,6 +55,26 @@ export default function ConGrid({ id }) {
                 {data.convention.images.map((a, i) => (
                     <ConGridItem key={i} position={i} data={a} />
                 ))}
+                <div className="flex flex-col w-full aspect-video p-2">
+                    <h5 className="font-semibold text-black/70">Share this Event</h5>
+                    <div className="w-full flex flex-wrap gap-2">
+                        <FacebookShareButton url={pathname}>
+                            <FacebookIcon size={32} className="rounded-md" />
+                        </FacebookShareButton>
+                        <TwitterShareButton url={pathname}>
+                            <TwitterIcon size={32} className="rounded-md" />
+                        </TwitterShareButton>
+                        <ViberShareButton url={pathname}>
+                            <ViberIcon size={32} className="rounded-md" />
+                        </ViberShareButton>
+                        <button
+                            onClick={() => { navigator.clipboard.writeText(pathname) }}
+                            className=" px-2 rounded-md square bg-gray-600 text-white flex items-center justify-center"
+                        >
+                            <Icon icon="material-symbols:link" size={32} />
+                        </button>
+                    </div>
+                </div>
             </div>
         </section >
     )
