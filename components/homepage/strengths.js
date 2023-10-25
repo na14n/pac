4e32a8b69f8c566dea9af7324bf3844706f625fb
na14n <na14an.07@gmail.com>
2023-10-25 17:58:59 +0800

    Animation Testing

--- components/homepage/strengths.js
+++ components/homepage/strengths.js
@@ -4,6 +4,10 @@ export const dynamic = 'force-dynamic'
 
 import { gql } from "@apollo/client";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { motion } from "framer-motion";
+import { Icon } from "@iconify-icon/react";
+import Image from "next/image";
+import { pTagRemover, sortByAttribute } from "@/lib/helpers";
 
 const query = gql` query FetchStrengths {
     homepageSections(where: {search: "strengths"}) {
@@ -23,11 +27,20 @@ const query = gql` query FetchStrengths {
   }
 `;
 
-import { Icon } from "@iconify-icon/react";
-import Image from "next/image";
-import { pTagRemover, sortByAttribute } from "@/lib/helpers";
 
-
+const item = {
+    hidden: {
+        opacity: 0,
+        x: 30,
+    },
+    show: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 1,
+        },
+    },
+}
 
 export default function Strengths() {
 
@@ -80,9 +93,18 @@ export default function Strengths() {
 
     return (strengths) ? (
         <div className="w-full h-fit xl:h-0 relative flex flex-col items-center justify-center z-40 xs:pb-16 2xl:px-48 lg:px-32 xs:px-4">
-            <div className="w-fit h-fit xl:absolute relative xl:-top-36 2xl:-top-32 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:gap-6 xl:gap-8 xs:gap-4 ">
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ staggerChildren: 0.5, delayChildren: 0.5 }}
+                className="w-fit h-fit xl:absolute relative xl:-top-36 2xl:-top-32 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 md:gap-6 xl:gap-8 xs:gap-4 "
+            >
                 {strengths.map((s, index) => (
-                    <div key={index} className="w-72 h-72 shrink-0 md:h-[16rem] xl:h-[16rem] 2xl:h-64 shadow-md bg-gradient-to-b odd:from-[#FAA541] odd:to-[#EE6400] even:from-[#FCFCFC] even:to-[#EFEFEF] rounded-md flex justify-start gap-2 flex-col items-center p-4 odd:text-[#FCFCFC] even:text-[#EE6400] peer group relative overflow-hidden">
+                    <motion.div
+                        initial={{opacity: 0, x: 30}}
+                        whileInView={{opacity: 1, x: 0}}
+                        className="w-72 h-72 shrink-0 md:h-[16rem] xl:h-[16rem] 2xl:h-64 shadow-md bg-gradient-to-b odd:from-[#FAA541] odd:to-[#EE6400] even:from-[#FCFCFC] even:to-[#EFEFEF] rounded-md flex justify-start gap-2 flex-col items-center p-4 odd:text-[#FCFCFC] even:text-[#EE6400] peer group relative overflow-hidden"
+                    >
                         {/* <Icon icon={s.icon} className="text-6xl" /> */}
                         <Image src={assets[index].sourceUrl} alt={assets[index].altText} height={60} width={60} className="object-contain object-center" />
                         <p className="w-full h-fit text-center text-xl lg:text-lg 2xl:text-xl font-bold peer-odd:text-[#EFEFEF] peer-even:text-[#FAA541]">
@@ -92,9 +114,9 @@ export default function Strengths() {
                             {pTagRemover(s.description)}
                             {/* {assets[index].sourceUrl} */}
                         </p>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
         </div>
     ) : (
         <div className="w-full h-fit lg:px-32 xs:px-4 relative flex flex-col items-center z-40 xs:pb-16 lg:pb-0">
