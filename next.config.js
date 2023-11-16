800cb39f181f5aaaa04b12eacf24c24f8086e3ad
na14n <106856208+na14n@users.noreply.github.com>
2023-11-16 13:47:24 +0800

    Update next.config.js
    - added web.prosapac.com as images domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['picsum.photos', 'prosapac.com']
+        domains: ['picsum.photos', 'prosapac.com', 'web.prosapac.com']
     },
     experimental: {
         serverActions: true,
