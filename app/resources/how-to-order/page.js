61ea1d2e2dc49cc857f84acb3c71be90f129985b
Ian Nicolas <na14an.07@gmail.com>
2024-02-21 16:50:33 +0800

    🔼 Step by Step Ordering Guide

--- app/resources/how-to-order/page.js
+++ app/resources/how-to-order/page.js
@@ -14,7 +14,7 @@ export default async function HowToOrder() {
                 <OrderingDescription />
                 <OrderingStepByStep />
                 {/* <QuotationGUide /> */}
-                {/* <OrderingGuide /> */}
+                <OrderingGuide />
                 {/* <Faq /> */}
             </div>
         </PageWrapper>
--- components/resources-pages/orderingGuide.js
+++ components/resources-pages/orderingGuide.js
@@ -2,9 +2,11 @@
 
 export const dynamic = 'force-dynamic'
 
+import { sortByAttribute } from "@/lib/helpers";
 import { gql } from "@apollo/client";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import parse from "html-react-parser"
+import Image from "next/image";
 import { useState } from 'react';
 
 const query = gql`
@@ -14,100 +16,23 @@ const query = gql`
                 title
                 sectionHeading
                 sectionSubheading
-                contentLine1
+            }
+        }
+        orderingOptions {
+            nodes {
+                id
+                order
+                name
+                orderingSteps
+                icon{
+                    sourceUrl
+                }
             }
         }
     }
 `
 
 const OrderingGuide = () => {
-
-    // const placeholder = [
-    //     {
-    //         title: 'Contact a Sales Agent',
-    //         steps: [
-    //             {
-    //                 title: 'Step One',
-    //                 description: 'Sales Agent Step One is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Two',
-    //                 description: 'Sales Agent Step Two is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Three',
-    //                 description: 'Sales Agent Step Three is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Four',
-    //                 description: 'Sales Agent Step Four is lorem ipsum dolor amet.'
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         title: 'Customer Support',
-    //         steps: [
-    //             {
-    //                 title: 'Step One',
-    //                 description: 'Customer Support Step One is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Two',
-    //                 description: 'Customer Support Step Two is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Three',
-    //                 description: 'Customer Support Step Three is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Four',
-    //                 description: 'Customer Support Step Four is lorem ipsum dolor amet.'
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         title: 'Facebook Messenger',
-    //         steps: [
-    //             {
-    //                 title: 'Step One',
-    //                 description: 'Facebook Messenger Step One is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Two',
-    //                 description: 'Facebook Messenger Step Two is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Three',
-    //                 description: 'Facebook Messenger Step Three is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Four',
-    //                 description: 'Facebook Messenger Step Four is lorem ipsum dolor amet.'
-    //             },
-    //         ],
-    //     },
-    //     {
-    //         title: 'Email',
-    //         steps: [
-    //             {
-    //                 title: 'Step One',
-    //                 description: 'Email Step One is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Two',
-    //                 description: 'Email Step Two is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Three',
-    //                 description: 'Email Step Three is lorem ipsum dolor amet.'
-    //             },
-    //             {
-    //                 title: 'Step Four',
-    //                 description: 'Email Step Four is lorem ipsum dolor amet.'
-    //             },
-    //         ],
-    //     },
-    // ]
 
     const { data } = useSuspenseQuery(
         query,
@@ -129,27 +54,39 @@ const OrderingGuide = () => {
 
 
     return (
-        <div className="w-full h-full flex flex-col justify-center 2xl:justify-start items-center bg-gradient-to-b from-[#F0892B]/90 to-[#E66204]/90 gap-8 py-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 2xl:py-32">
-            <h1 className="w-fit h-fit text-3xl 2xl:text-5xl text-[#FCFCFC] font-bold uppercase text-center">
-                Step-by-Step Ordering Guide
-            </h1>
-            <div className="grid grid-cols-1 max-md:w-full md:grid-cols-2 lg:grid-cols-4 bg-[#F1F1F1] py-2 rounded-md divide-x-[2px] divide-[#E1E1E1] border-2 border-[#E1E1E1] mb-8">
-                {data?.contactUsContents?.nodes[0]?.sectionSubheading.map((s, index) => (
-                    <button key={index} className={` px-4 lg:max-w-[180px]`} onClick={() => handleTabClick(index)}>
-                        <p className={`uppercase h-full px-6 py-2 rounded-md font-bold lg:text-sm flex justify-center items-center transition-all ${selectedTab === index ? `bg-nav-orange text-[#FCFCFC]` : `text-[#575757]/50 hover:text-nav-orange`}`}>
-                            {s}
+        <div className="w-full h-full flex flex-col justify-center 2xl:justify-start items-center bg-gradient-to-b from-[#F9A03C] to-[#EF6703] gap-8 xl:gap-12 py-12 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 2xl:py-32">
+            <div className="flex flex-col items-center justify-center gap-2">
+                <h1 className="w-fit h-fit text-3xl 2xl:text-6xl text-white font-bold uppercase text-center">
+                    {data?.contactUsContents?.nodes[0]?.sectionHeading}
+                </h1>
+                <h3 className="w-fit h-fit text-xl text-[#FCFCFC] text-center">
+                    {data?.contactUsContents?.nodes[0]?.sectionSubheading}
+                </h3>
+            </div>
+            <div className="flex flex-wrap gap-4 bg-[#F1F1F1] p-2 rounded-md mb-8">
+                {sortByAttribute(data?.orderingOptions?.nodes, "order").map((option, index) => (
+                    <button key={index} className={`p-4 gap-4 rounded-md lg:w-[160px] flex flex-col items-center ${selectedTab === index ? `bg-nav-orange text-[#FCFCFC]` : `text-[#575757]/50 hover:text-nav-orange`}`} onClick={() => handleTabClick(index)}>
+                        <div className="w-1/3 aspect-square relative">
+                            <Image fill src={option?.icon?.sourceUrl} className="object-contain object-center" />
+                        </div>
+                        <p className={`uppercase rounded-md font-bold lg:text-sm flex justify-center items-center transition-all`}>
+                            {option.name}
                         </p>
                     </button>
                 ))}
             </div>
-            {/* <div className="w-full md:px-16 grid grid-cols-1 md:grid-cols-2 gap-8 2xl:gap-12">
-                {selectedTab.steps.map((s, index) => (
-                    
+            <div className="w-full xl:w-2/3 flex flex-col rounded-md overflow-hidden">
+                {sortByAttribute(data?.orderingOptions?.nodes, "order")[selectedTab].orderingSteps.map((step, index) => (
+                    <div key={index} className="w-full h-fit even:bg-[#FFFFFF] odd:bg-[#EBEBEB] flex items-center justify-center px-8">
+                        <div className="w-fit h-full flex gap-2 items-center justify-center p-2 text-xl">
+                            <p className="text-[#FF852F] font-bold">Step</p>
+                            <div className="bg-[#FF852F] text-white font-bold h-fit p-4 flex items-center justify-center rounded-full relative">
+                                <span className="absolute">{index + 1}</span>
+                            </div>
+                        </div>
+                        <span className="w-full b p p-4 p b a">{parse(step)}</span>
+                    </div>
                 ))}
-
-            </div> */}
-            <div className="w-full h-fit b p rounded-md bg-white p-4">
-                {parse(data?.contactUsContents?.nodes[0]?.contentLine1[selectedTab])}
             </div>
         </div>
     )
