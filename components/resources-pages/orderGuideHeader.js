da6ef65ed183082f17effba0b16d9a225c815f2f
Ian Nicolas <na14an.07@gmail.com>
2024-03-05 13:34:17 +0800

    🔼 Order Page Header Layout Fix

--- components/resources-pages/orderGuideHeader.js
+++ components/resources-pages/orderGuideHeader.js
@@ -40,7 +40,7 @@ export default function OrderGuideHeader() {
         <section className="relative w-full h-fit aspect-[2/2] md:aspect-[16/4] flex items-center justify-center overflow-hidden z-0">
             <div className="absolute z-0 t-0 bg-[#ff6100] w-full h-full"></div>
             <div className="w-full h-full z-10 flex max-md:flex-col-reverse items-center justify-center max-md:mt-40">
-                <div className="self-stretch w-full h-full relative mt-4">
+                <div className="self-stretch w-full h-full relative max-md:mt-4">
                     <Image fill src={data?.contactUsContents?.nodes[0]?.mediaLine1[0] ? data?.contactUsContents?.nodes[0]?.mediaLine1[0]?.sourceUrl : 'https://picsum.photos/1920/1080'} className='object-cover md:object-cover object-left-bottom md:object-center' alt="dental-website-banner" />
                 </div>
                 <div className="md:w-1/2 shrink-0 flex flex-col md:gap-2 items-center justify-center">
