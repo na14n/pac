534452bfb61d4e32db1ca92df6f447d15ef366d1
Ian Nicolas <na14an.07@gmail.com>
2024-03-05 13:12:28 +0800

    🔼 Order Header Banner Update

--- components/resources-pages/orderGuideHeader.js
+++ components/resources-pages/orderGuideHeader.js
@@ -37,17 +37,17 @@ export default function OrderGuideHeader() {
     );
 
     return (
-        <section className="relative w-full h-fit aspect-[2/2] md:aspect-[16/5] flex items-center justify-center overflow-hidden z-0">
+        <section className="relative w-full h-fit aspect-[2/2] md:aspect-[16/4] flex items-center justify-center overflow-hidden z-0">
             <div className="absolute z-0 t-0 bg-[#ff6100] w-full h-full"></div>
             <div className="w-full h-full z-10 flex max-md:flex-col-reverse items-center justify-center max-md:mt-48">
                 <div className="self-stretch w-full h-full relative">
-                    <Image fill src={data?.contactUsContents?.nodes[0]?.mediaLine1[0] ? data?.contactUsContents?.nodes[0]?.mediaLine1[0]?.sourceUrl : 'https://picsum.photos/1920/1080'} className='object-contain object-left-bottom md:object-left' alt="dental-website-banner" />
+                    <Image fill src={data?.contactUsContents?.nodes[0]?.mediaLine1[0] ? data?.contactUsContents?.nodes[0]?.mediaLine1[0]?.sourceUrl : 'https://picsum.photos/1920/1080'} className='object-contain md:object-cover object-left-bottom md:object-center' alt="dental-website-banner" />
                 </div>
-                <div className="md:w-1/2 shrink-0 flex flex-col md:gap-2 items-center">
-                    <h2 className="text-[#FCFCFC] text-3xl xl:text-5xl uppercase w-fit">
+                <div className="md:w-1/2 shrink-0 flex flex-col md:gap-2 items-center justify-center">
+                    <h2 className="text-[#FCFCFC] text-3xl xl:text-5xl 2xl:text-7xl uppercase w-fit">
                         {data?.contactUsContents?.nodes[0] ? data?.contactUsContents?.nodes[0]?.sectionHeading : 'Nationwide'}
                     </h2>
-                    <h1 className="font-bold text-[#FCFCFC] text-4xl xl:text-6xl tracking-wide uppercase w-fit">
+                    <h1 className="font-bold text-[#FCFCFC] text-4xl xl:text-6xl 2xl:text-8xl tracking-wide uppercase w-fit">
                         {data?.contactUsContents?.nodes[0] ? data?.contactUsContents?.nodes[0]?.sectionSubheading : 'Delivery'}
                     </h1>
                 </div>
